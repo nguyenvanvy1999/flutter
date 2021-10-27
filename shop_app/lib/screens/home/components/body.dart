48ff815042ecf8f495453b286c3ddba13ab24aa7
hung_tranduc <hungtranduc@trulet.com>
2021-10-27 17:04:36 +0700

    init : init project

---
+++ shop_app/lib/screens/home/components/body.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:getx/constants.dart';
+import 'package:getx/models/product.dart';
+import 'package:getx/screens/details/details_screen.dart';
+
+import 'categorries.dart';
+import 'item_card.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: Text(
+            'Women',
+            style: Theme.of(context)
+                .textTheme
+                .headline5!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        const Categories(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: GridView.builder(
+                itemCount: products.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: kDefaultPadding,
+                  crossAxisSpacing: kDefaultPadding,
+                  childAspectRatio: 0.75,
+                ),
+                itemBuilder: (context, index) => ItemCard(
+                      product: products[index],
+                      press: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailsScreen(
+                              product: products[index],
+                            ),
+                          )),
+                    )),
+          ),
+        ),
+      ],
+    );
+  }
+}
