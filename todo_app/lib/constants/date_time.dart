b00b70267baa6ed3aca9a63349f3236162bb9a97
hung_tranduc <hungtranduc@trulet.com>
2021-10-30 18:23:22 +0700

    add : constants

---
+++ todo_app/lib/constants/date_time.dart
@@ -0,0 +1,4 @@
+const int startYear = 2015;
+const int endYear = 2121;
+const List<int> remindList = [5, 10, 15, 20, 25, 30];
+const List<String> repeatList = ['None', 'Daily', 'Weekly', 'Monthly'];
--- todo_app/lib/constants/text_style.dart
+++ todo_app/lib/constants/text_style.dart
@@ -26,3 +26,11 @@ TextStyle get titleStyle {
           fontWeight: FontWeight.w400,
           color: Get.isDarkMode ? Colors.white : Colors.black));
 }
+
+TextStyle get subTitleStyle {
+  return GoogleFonts.lato(
+      textStyle: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          color: Get.isDarkMode ? Colors.grey[100] : Colors.grey[700]));
+}
