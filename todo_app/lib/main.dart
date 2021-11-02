bcfd243d7e49fe105d87fab4b92944c02d402a5e
hung_tranduc <hungtranduc@trulet.com>
2021-11-02 09:07:27 +0700

    edit : add packages

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:todo_app/constants/themes.dart';
+import 'package:todo_app/db/db_helper.dart';
 import 'package:todo_app/pages/home/home_page.dart';
 import 'package:todo_app/services/theme_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await DBHelper.initDb();
   await GetStorage.init('theme');
   await GetStorage.init('date');
   runApp(const MyApp());
